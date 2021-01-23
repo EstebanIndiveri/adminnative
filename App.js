d9d03241c1ba273379c606f2cb35521803d71837
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-01-23 20:49:21 -0300

    reveral status

---
+++ App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <Text>algo</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ views/Inicio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+
+
+const Inicio = () => {
+    return ( 
+        <View>
+            <Text>algo</Text>
+        </View>
+     );
+}
+ 
+export default Inicio;
